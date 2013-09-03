a767667b3f62b6dbece7e9ee40f7b3954cab2572
cway <cway@knock-pro.local>
2013-09-03 22:21:15 +0800

    x

--- models/account.rb
+++
@@ -1,9 +0,0 @@
-#encoding: utf-8
-#author cway 2013-6-29
-
-class Account < ActiveRecord::Base
-  #acts_as_cached
-  attr_accessible :id, :name, :password, :wechat_id, :balance
-  
-  validates :name,       :presence => true
-end
--- models/sales_order_item.rb
+++
@@ -1,8 +0,0 @@
-#encoding: utf-8
-#author andy 2013-08-20
-
-class SalesOrderItem < ActiveRecord::Base
-  # attr_accessor :thumb
-  attr_accessible :item_id, :order_id, :created_at, :updated_at, :product_id, :weight, :is_virtual, :sku, :name, :description, :applied_rule_ids, :qty_orderd, :qty_shipped, :base_cost, :base_price, :total_price
-  self.table_name = "sales_order_items"
-end
